e4ce8ccf644122b34bec9195a0595d199d17a1f3
Adriavi <88682997+Adriavi@users.noreply.github.com>
2022-02-26 01:00:02 -0500

    Add files via upload

---
+++ UX_UI_HW_19/index.js
@@ -0,0 +1,12 @@
+ 
+ 
+ //this is mi first JavaScript code 
+ console.log(' HOLA ADRIANA your file is super new ');
+
+ $(function() {
+   $("#button1").hide(300).show(1000);
+ });
+
+ $("#hideMe").click( function(){
+  $('.animate').animate( {left: '250px'}, 1000 );
+});
---
+++ UX_UI_HW_19/js/index.js
@@ -0,0 +1,8 @@
+//this is mi first JavaScript code 
+console.log(' HOLA ADRIANA your file is new ');
+
+$("h1").css("color","red");
+
+$(".resume").css("color","blue");
+
+$("headline").html("Hello");
