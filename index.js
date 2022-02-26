58b4cf10f010db685446fb080c3d25668ce198eb
Adriavi <adrianavilafoto@gmail.com>
2022-02-26 00:50:51 -0500

    update hw19

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  //this is mi first JavaScript code 
  console.log(' HOLA ADRIANA your file is super new ');
 
- $('#button1').on("click", function(){
-    $('.hideMe').slideToggle();
-  });
+ $(function() {
+   $("#button1").hide(300).show(1000);
+ });
+
+ $("#hideMe").click( function(){
+  $('.animate').animate( {left: '250px'}, 1000 );
+});
